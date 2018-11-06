28cdf0675bde86ebbd7c71d2525bbde60285dea1
FXSZ <44221375+FXSZ@users.noreply.github.com>
2018-11-06 15:30:18 +0100

    + SQL erweitert

--- sql/Meilenstein2.sql
+++ sql/Meilenstein2.sql
@@ -1,5 +1,5 @@
-DROP DATABASE IF EXISTS Meilenstein2;
-CREATE DATABASE Meilenstein2; 
+-- DROP DATABASE IF EXISTS Meilenstein2;
+-- CREATE DATABASE Meilenstein2; 
 USE Meilenstein2;
 
 			-- DROP TABLE IF EXISTS Kunden;
@@ -15,12 +15,12 @@ USE Meilenstein2;
 -- =======================================================
 DROP TABLE IF EXISTS Benutzer;
 CREATE TABLE Benutzer(
-	`Nummer` INT NOT NULL AUTO_INCREMENT,
+	`Nummer` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
 	-- `NAME`
 		`Vorname` VARCHAR(100) NOT NULL,			
 		`Nachname` VARCHAR(50) NOT NULL,
-	`Aktiv` tinyint(1) NOT NULL DEFAULT 0,
-	`Anlegedatum` DATETIME NOT NULL DEFAULT curdate(), -- oder besser CURRENT_DATE ?
+	`Aktiv` TINYINT(1) NOT NULL DEFAULT 0,
+	`Anlegedatum` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	-- `Auth`
 		`Salt` VARCHAR(32) NOT NULL,
 		`Hash` VARCHAR(24) NOT NULL,
@@ -28,27 +28,41 @@ CREATE TABLE Benutzer(
 	`Letzter Login` DATETIME DEFAULT NULL, -- ist optional
 	`E-Mail` VARCHAR(255) NOT NULL,
 	`Geburtsdatum` DATE, -- ist Optional
+	`Alter` INT(3) AS (year(CURRENT_TIMESTAMP) - year(`Geburtsdatum`)),
 	PRIMARY KEY (`Nummer`),
   	UNIQUE KEY (`E-mail`,`Nutzername`)
 );
+DESCRIBE Benutzer;
+
 
 DROP TABLE IF EXISTS Bestellungen;
 CREATE TABLE Bestellungen(
-	`Nummer` INT NOT NULL AUTO_INCREMENT,
-	`Abholzeitpunkt` DATETIME DEFAULT NULL, -- ist optional!
-	`Bestellzeitpunkt` DATETIME NOT NULL DEFAULT curdate(), -- oder besser CURRENT_DATE ?
+	`Nummer` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`Abholzeitpunkt` DATETIME DEFAULT NULL, -- ist optional! --ToDo: Muss wenn er angegeben wird später als der Bestellzeitpunkt
+	`Bestellzeitpunkt` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, -- oder besser CURRENT_DATE ?
+	-- `Endpreis` DECIMAL --ToDo ! -- mehr als 99,99€
 	PRIMARY KEY (`Nummer`)
 );
+DESCRIBE Bestellungen;
 
--- BIS HIERHIN..
 
 DROP TABLE IF EXISTS Mahlzeiten;
 CREATE TABLE Mahlzeiten(
+	`ID` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	`Beschreibung` VARCHAR(1024) NOT NULL,
+	`Verfügbar` TINYINT(1) NOT NULL Default 0,
+	`Vorrat` INT UNSIGNED NOT NULL DEFAULT 0,
+	PRIMARY KEY(`ID`)
 );
+DESCRIBE Mahlzeiten;
 
 DROP TABLE IF EXISTS Deklarationen;
 CREATE TABLE Deklarationen(
+	`Zeichen` VARCHAR(2) NOT NULL,
+	`Beschriftung` VARCHAR(32) NOT NULL,
+	PRIMARY KEY(`Zeichen`)
 );
+DESCRIBE Deklarationen;
 
 DROP TABLE IF EXISTS Preise;
 CREATE TABLE Preise(
@@ -78,6 +92,8 @@ DROP TABLE IF EXISTS Deklarationen;
 CREATE TABLE Deklarationen(
 );
 
+-- Ablaufdatum standardmäßig eine Woche in der Zukunft
+-- der Grund wird nicht länger als 255 Zeichen lang
 DROP TABLE IF EXISTS Gäste;
 CREATE TABLE Gäste(
 );
